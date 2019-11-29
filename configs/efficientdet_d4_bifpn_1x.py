07a604b530fdc4e43582bf2bd0731e8aa5231036
liuxiang.cd <liuxiang.cd@ccbft.com>
2019-11-29 10:09:59 +0800

    fix padding error

--- configs/efficientdet_d4_bifpn_1x.py
+++ configs/efficientdet_d4_bifpn_1x.py
@@ -59,7 +59,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    dict(type='Pad', size_divisor=128),#32->128
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
@@ -73,7 +73,7 @@ test_pipeline = [
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
+            dict(type='Pad', size_divisor=128),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
